f37b500ca710b0cece82622da517e94b5264fa9e
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-06-13 11:53:24 +0100

    modify 'Repository' to contain more basic operations

--- useCase/src/main/java/com/example/g14/coalesce/usecase/Repository.kt
+++ useCase/src/main/java/com/example/g14/coalesce/usecase/Repository.kt
@@ -1,18 +1,26 @@
 package com.example.g14.coalesce.usecase
 
+import com.example.g14.coalesce.entity.IdType
 import com.example.g14.coalesce.entity.Message
+import com.example.g14.coalesce.entity.User
+import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.Single
 
 /**
  * Created by Gabriel Fortin
  */
 
-// TODO: move to package "com.example.g14.coalesce"?
+/** IMPORTANT:
+ * Implementors of this class should ensure that all rx results yield events
+ * on subscription as quickly as possible
+ * e.g. by using .startWith()
+ */
 interface Repository {
-    fun getActiveUser(): GetActiveUser
+    fun getCurrentUserId(): Observable<IdType?>
+    fun setCurrentUserId(id: IdType): Completable
+    fun getUserBy(id: IdType): Single<User>
 
-    fun getMessages(): GetMessages
-
-    fun getMoreMessages(beforeDate: Long, quantity: Int = 5): GetMessages
+    fun getMessages(groupId: IdType, quantity: Int, before: Long?): Observable<List<Message>>
+    fun addMessage(message: Message): Completable
 }
-
