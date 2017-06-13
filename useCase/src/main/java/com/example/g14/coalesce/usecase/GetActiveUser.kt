0e4f31264e84b37eb0e46dddd0d670e71db840cd
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-06-13 11:54:08 +0100

    implement 'GetActiveUser' use case

--- useCase/src/main/java/com/example/g14/coalesce/usecase/GetActiveUser.kt
+++ useCase/src/main/java/com/example/g14/coalesce/usecase/GetActiveUser.kt
@@ -1,21 +1,31 @@
 package com.example.g14.coalesce.usecase
 
+import com.example.g14.coalesce.entity.IdType
 import com.example.g14.coalesce.entity.User
+import com.example.g14.coalesce.usecase.ActiveUserResult.*
 import io.reactivex.Observable
 
 /**
  * Created by Gabriel Fortin
  */
 
-interface GetActiveUser: ObservableUseCase<ActiveUserResult>{
+class GetActiveUser(val repo: Repository): ObservableUseCase<ActiveUserResult> {
 
-    override fun execute(): Observable<ActiveUserResult> {
-        TODO("not implemented")
-    }
+    override fun execute(): Observable<ActiveUserResult> =
+            repo
+            .getCurrentUserId()
+            .flatMap { when (it) {
+                    is IdType -> repo
+                            .getUserBy(it)
+                            .map { user -> LoggedIn(user) }
+                            .toObservable()
+                    else -> Observable.just(NoUser())
+                }
+            }
 
 }
 
-sealed class ActiveUserResult() {
+sealed class ActiveUserResult {
     class NoUser : ActiveUserResult()
     class LoggedIn(val user: User): ActiveUserResult()
 }
