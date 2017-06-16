b3259fad1d9eb03b4546583a92b3ef7a4e33bd3c
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-06-16 10:45:48 +0100

    use null-carrying in 'GetActiveGroup'

--- useCase/src/main/java/com/example/g14/coalesce/usecase/GetActiveGroup.kt
+++ useCase/src/main/java/com/example/g14/coalesce/usecase/GetActiveGroup.kt
@@ -19,47 +19,21 @@ class ActiveGroup(val group: Group): ActiveGroupResult()
 class NoGroup(): ActiveGroupResult()
 
 
-const val TRANSFORM_TO_NO_GROUP: String = "no group"
-
 /** This implementation assumes a user can belong to only one group */
-class GetTheOnlyActiveGroup(
-        val repo: Repository
-) : GetActiveGroup {
+class GetTheOnlyActiveGroup(val repo: Repository) : GetActiveGroup {
 
     override fun execute(): Observable<ActiveGroupResult> =
         repo
         .getCurrentUserId()
-        .map(this::transformNullToNullException)
-        .flatMap(repo::getGroupsFor)
-        .map { listOfGroups ->
-            when (listOfGroups.size) {
-                0 -> NoGroup()
-                1 -> ActiveGroup(listOfGroups[0])
-                else -> selectActiveGroup(listOfGroups)
-            }
-        }
-        .compose(transformNullExceptionToNoGroup)
-
-    private fun transformNullToNullException(maybeId: IdType?): IdType =
-            maybeId ?: throw NullPointerException(TRANSFORM_TO_NO_GROUP)
-
-    private object transformNullExceptionToNoGroup :
-            ObservableTransformer<ActiveGroupResult, ActiveGroupResult> {
-        override fun apply(upstream: Observable<ActiveGroupResult>):
-                Observable<ActiveGroupResult> =
-            upstream
-            .materialize()
-            .map {
-                if (it.isOnError && it.error.message == TRANSFORM_TO_NO_GROUP) {
-                    Notification.createOnNext(NoGroup())
-                } else {
-                    it
-                }
-            }
-            .dematerialize<ActiveGroupResult>()
-    }
+        .magicallyHideNull()
+            .flatMap(repo::getGroupsFor)
+            .map(this::selectActiveGroup)
+        .magicallyReplaceHiddenNullWith(NoGroup())
 
     protected fun selectActiveGroup(listOfGroups: List<Group>): ActiveGroupResult {
+        if (listOfGroups.size == 0) return NoGroup()
+        if (listOfGroups.size == 1) return ActiveGroup(listOfGroups[0])
+
         val msg = "cannot select active group; not implemented for >1 groups"
         throw UnsupportedOperationException(msg)
     }
