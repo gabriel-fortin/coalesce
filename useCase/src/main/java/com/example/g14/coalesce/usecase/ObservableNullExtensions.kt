54025b3cf07a904b655cced25223b947177e53fe
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-06-18 22:17:41 +0100

    simplify null-carrying

--- useCase/src/main/java/com/example/g14/coalesce/usecase/ObservableNullExtensions.kt
+++ useCase/src/main/java/com/example/g14/coalesce/usecase/ObservableNullExtensions.kt
@@ -7,7 +7,7 @@ import io.reactivex.Observable
  * Created by Gabriel Fortin
  */
 
-class NullCarrier : Throwable()
+object NULL_CARRIER : Throwable()
 
 fun <T, R> Observable<T?>.whenNullThen(
         specialValue: R,
@@ -19,13 +19,13 @@ fun <T, R> Observable<T?>.whenNullThen(
         .compose { transformHiddenNullTo(specialValue, it) }
 
 private inline fun <T> Observable<T?>.magicallyHideNull(): Observable<T> =
-        this.map { it ?: throw NullCarrier() }
+        this.map { it ?: throw NULL_CARRIER }
 
 private inline fun <R> transformHiddenNullTo(specialValue: R, obs: Observable<R>): Observable<R> =
         obs
         .materialize()
         .map<Notification<*>> { notification ->
-            if (notification.isOnError && notification.error is NullCarrier) {
+            if (notification.isOnError && notification.error == NULL_CARRIER) {
                 Notification.createOnNext(specialValue)
             } else {
                 notification
