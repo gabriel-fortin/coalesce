f739759ffcff94c9a2db1c9a93c3e725f01d36c1
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-06-25 12:41:19 +0100

    refactor 'GetGroupsTest' for adding more tests

--- useCase/src/test/kotlin/com/example/g14/coalesce/usecase/group/GetGroupsTest.kt
+++ useCase/src/test/kotlin/com/example/g14/coalesce/usecase/group/GetGroupsTest.kt
@@ -6,6 +6,7 @@ import com.example.g14.coalesce.entity.User
 import com.example.g14.coalesce.usecase.Repository
 import com.example.g14.coalesce.usecase.user.ActiveUserResult
 import com.example.g14.coalesce.usecase.user.GetActiveUser
+import io.reactivex.Observable
 import io.reactivex.subjects.BehaviorSubject
 import org.junit.Before
 import org.junit.Test
@@ -26,47 +27,60 @@ class GetGroupsTest {
     val u4 = User(104, "user 104", "")
     val u99 = User(199, "user 199", "")
 
-//    fun makeSuccess(groupId: IdType, members: User...)
+    lateinit var repoMock: Repository
+    lateinit var repoGetGroupsForMapping: Map<IdType, Group>
 
+    lateinit var activeUserUseCaseMock: GetActiveUser
+    lateinit var activeUserStream: BehaviorSubject<ActiveUserResult>
 
     @Before
     fun setUp() {
+        prepareRepoMock()
+        prepareActiveUserUseCase()
+    }
+
+    fun makeGroup(groupId: IdType, vararg members: User) =
+            Group(groupId, members.asList())
+
+    fun prepareRepoMock() {
+        repoMock = mock(Repository::class.java)
+        `when`(repoMock.getGroupsFor(any(IdType::class.java))).thenAnswer { invoc ->
+            val group: Group =
+                    // pack argument into list to process it
+                    listOf(invoc.getArgument<IdType>(0))
+                            // find group for requested id
+                            .map { repoGetGroupsForMapping[it] }
+                            // make sure it was found
+                            .map { it ?: throw NullPointerException("missing mapping entry") }
+                            // extract the result from list
+                            .get(0)
+            BehaviorSubject
+                    .create<List<Group>>()
+                    .apply { onNext(listOf(group)) }
+        }
+    }
+
+    fun prepareActiveUserUseCase() {
+        activeUserStream = BehaviorSubject.create<ActiveUserResult>()
+                .apply { doOnNext {println("active user stream::onNext: $it")} }
+                .apply { doOnComplete { println("active user stream::onComplete")} }
+                .apply { doOnError { println("active user stream::onError")} }
+                .apply { doOnSubscribe { println("active user stream::onSubscribe")} }
+        activeUserUseCaseMock = mock(GetActiveUser::class.java)
+        `when`(activeUserUseCaseMock.execute()).thenReturn(activeUserStream)
     }
 
     @Test
     fun execute_simpleLists() {
         // PREPARE
-        val activeUserStream = BehaviorSubject.create<ActiveUserResult>()
-                .apply { doOnNext {println("active user stream::onNext: $it")} }
-                .apply { doOnComplete { println("active user stream::onComplete")} }
-                .apply { doOnError { println("active user stream::onError")} }
-                .apply { doOnSubscribe { println("active user stream::onSubscribe")} }
-        val activeUserUseCase: GetActiveUser = mock(GetActiveUser::class.java)
-        `when`(activeUserUseCase.execute()).thenReturn(activeUserStream)
-
-        val repo: Repository = mock(Repository::class.java)
-        `when`(repo.getGroupsFor(any(IdType::class.java))).thenAnswer { invoc ->
-            val groupMappingFor = mapOf(
-                    101 to Group(51, listOf(u1)),
-                    102 to Group(52, listOf(u2, u99)),
-                    103 to Group(53, listOf(u3_bis)),
-                    104 to Group(54, listOf(u4, u99)))
-            val groupResult: Group =
-                    // pack argument into list to process it
-                    listOf(invoc.getArgument<IdType>(0))
-                    // find group for requested id
-                    .map { groupMappingFor[it] }
-                    // make sure it was found
-                    .map { it ?: throw NullPointerException("missing mapping") }
-                    // extract the result
-                    .get(0)
-            BehaviorSubject
-                    .create<List<Group>>()
-                    .apply { onNext(listOf(groupResult)) }
-        }
+        repoGetGroupsForMapping = mapOf(
+                101 to makeGroup(51, u1),
+                102 to makeGroup(52, u2, u99),
+                103 to makeGroup(53, u3),
+                104 to makeGroup(54, u4, u99))
 
         // EXECUTE
-        val testObserver = GetGroupsImpl(repo, activeUserUseCase)
+        val testObserver = GetGroupsImpl(repoMock, activeUserUseCaseMock)
                 .execute()
                 .test()
 
@@ -79,22 +93,27 @@ class GetGroupsTest {
 
         // VERIFY
         testObserver.assertValues(
-                GroupsResult.Success(listOf(Group(51, listOf(u1)))),
-                GroupsResult.Success(listOf(Group(52, listOf(u2, u99)))),
-                GroupsResult.Success(listOf(Group(53, listOf(u3)))),
-                GroupsResult.Success(listOf(Group(54, listOf(u4, u99))))
+                GroupsResult.Success(listOf(makeGroup(51, u1))),
+                GroupsResult.Success(listOf(makeGroup(52, u2, u99))),
+                // here, we compare to an equal element 'u3_bis'
+                GroupsResult.Success(listOf(makeGroup(53, u3_bis))),
+                GroupsResult.Success(listOf(makeGroup(54, u4, u99)))
         )
         testObserver.assertNotTerminated()
 
-        verify(repo).getGroupsFor(101)
-        verify(repo).getGroupsFor(102)
-        verify(repo).getGroupsFor(103)
-        verify(repo).getGroupsFor(104)
-        verifyNoMoreInteractions(repo)
+        verifyRepoQueries(101..104)
+        verifyActiveUserInteractions()
+    }
+    fun verifyRepoQueries(ids: Iterable<IdType>) {
+        ids.forEach {
+            verify(repoMock).getGroupsFor(it)
+        }
+        verifyNoMoreInteractions(repoMock)
+    }
 
-        verify(activeUserUseCase).execute()
-        verifyNoMoreInteractions(activeUserUseCase)
-
+    fun verifyActiveUserInteractions() {
+        verify(activeUserUseCaseMock).execute()
+        verifyNoMoreInteractions(activeUserUseCaseMock)
     }
 
 }
