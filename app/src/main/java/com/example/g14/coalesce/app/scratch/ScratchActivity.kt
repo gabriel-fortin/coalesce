585b30b9cfb028a51e96603df0b08e25123aae3c
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-09-14 20:26:43 +0100

    add swiping using 'ItemTouchHelper'

--- app/src/main/java/com/example/g14/coalesce/app/scratch/ScratchActivity.kt
+++ app/src/main/java/com/example/g14/coalesce/app/scratch/ScratchActivity.kt
@@ -1,8 +1,14 @@
 package com.example.g14.coalesce.app.scratch
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Canvas
+import android.graphics.Rect
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
+import android.util.Log
+import android.view.View
 import com.example.g14.coalesce.app.R
 import kotlinx.android.synthetic.main.activity_scratch.*
 
@@ -15,6 +21,51 @@ class ScratchActivity : AppCompatActivity() {
         shoppingRecycler.adapter = BambooAdapter(SAMPLE_DATA, this)
         shoppingRecycler.layoutManager = LinearLayoutManager(this)
                 .apply { orientation = LinearLayoutManager.VERTICAL }
+//        shoppingRecycler.addItemDecoration()
+        BambooTouchHelper(BambooCallback()).attachToRecyclerView(shoppingRecycler)
+    }
+
+    class BambooCallback : ItemTouchHelper.Callback() {
+        override fun getMovementFlags(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?): Int {
+            return makeMovementFlags(0, ItemTouchHelper.RIGHT)
+        }
+
+        override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?): Boolean {
+            TODO("not implemented: ${javaClass.simpleName}.onMove")
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            Log.i("AAA", "SWIPE")
+        }
+    }
+
+    class BambooTouchHelper(callback: ItemTouchHelper.Callback) : ItemTouchHelper(callback) {
+    }
+
+    class BambooItemDecor : RecyclerView.ItemDecoration() {
+        override fun onDrawOver(c: Canvas?, parent: RecyclerView?, state: RecyclerView.State?) {
+            super.onDrawOver(c, parent, state)
+        }
+
+        override fun onDrawOver(c: Canvas?, parent: RecyclerView?) {
+            super.onDrawOver(c, parent)
+        }
+
+        override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+            super.onDraw(c, parent, state)
+        }
+
+        override fun onDraw(c: Canvas?, parent: RecyclerView?) {
+            super.onDraw(c, parent)
+        }
+
+        override fun getItemOffsets(outRect: Rect?, itemPosition: Int, parent: RecyclerView?) {
+            super.getItemOffsets(outRect, itemPosition, parent)
+        }
+
+        override fun getItemOffsets(outRect: Rect?, view: View?, parent: RecyclerView?, state: RecyclerView.State?) {
+            super.getItemOffsets(outRect, view, parent, state)
+        }
     }
 
     data class DataItem(val title: String, val prio: Int, val bought: Boolean)
