ffce0e0de712058a9dbbf12e5eddb6a810a0c4c5
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-09-17 15:15:25 +0100

    add custom swiping in RecyclerView

--- app/src/main/java/com/example/g14/coalesce/app/scratch/MyViewHolder.kt
+++ app/src/main/java/com/example/g14/coalesce/app/scratch/MyViewHolder.kt
@@ -2,14 +2,28 @@ package com.example.g14.coalesce.app.scratch
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.TextView
+import android.widget.Toast
 import com.example.g14.coalesce.app.R
 
 class BambooViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val prioTextTV = view.findViewById(R.id.priorityText) as TextView
     private val titleTextTV = view.findViewById(R.id.titleText) as TextView
     private val checkBoxCB = view.findViewById(R.id.buyingStateBox) as CheckBox
+    private val itemData = view.findViewById(R.id.itemData) as ViewGroup
+
+
+    init {
+        view.findViewById(R.id.moveToSortButton).setOnClickListener {
+            Toast.makeText(view.context, "sort CLICK", Toast.LENGTH_SHORT).show()
+        }
+        view.findViewById(R.id.removeButton).setOnClickListener {
+            Toast.makeText(view.context, "remove CLICK", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
     var priorityText: String
         set(value) { prioTextTV.text = value }
--- app/src/main/java/com/example/g14/coalesce/app/scratch/ScratchActivity.kt
+++ app/src/main/java/com/example/g14/coalesce/app/scratch/ScratchActivity.kt
@@ -5,8 +5,6 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
 import android.os.Bundle
-import android.support.v4.graphics.ColorUtils
-import android.support.v4.view.ViewCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -27,7 +25,7 @@ class ScratchActivity : AppCompatActivity() {
                 .apply { orientation = LinearLayoutManager.VERTICAL }
 
         shoppingRecycler.addItemDecoration(BambooItemDecor())
-        ItemTouchHelper(BambooCallback()).attachToRecyclerView(shoppingRecycler)
+//        ItemTouchHelper(BambooCallback()).attachToRecyclerView(shoppingRecycler)
     }
 
 
