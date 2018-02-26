c28cd7e7042ddf68ba88b32802ca15d43f200f6c
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2018-02-26 20:20:07 +0000

    add proof-of-concept separator in recycler view

--- app/src/main/java/com/example/g14/coalesce/app/scratch/ScratchActivity.kt
+++ app/src/main/java/com/example/g14/coalesce/app/scratch/ScratchActivity.kt
@@ -1,9 +1,6 @@
 package com.example.g14.coalesce.app.scratch
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -13,7 +10,6 @@ import android.util.Log
 import android.view.View
 import com.example.g14.coalesce.app.R
 import com.example.g14.coalesce.app.shoppinglist.ShopItemAdapter
-import com.example.g14.coalesce.app.shoppinglist.ShopItemViewHolder
 import com.example.g14.coalesce.app.shoppinglist.SwipeItemForOptions
 import kotlinx.android.synthetic.main.activity_scratch.*
 
@@ -43,8 +39,13 @@ class ScratchActivity : AppCompatActivity() {
         }
 
         override fun getItemOffsets(outRect: Rect, view: View?, parent: RecyclerView?, state: RecyclerView.State?) {
-            outRect.top = 13
-            outRect.bottom = 25
+            val childAdapterPosition = parent?.getChildAdapterPosition(view) ?: throw Exception("no adapter position :(")
+//            parent.getChildViewHolder(view)
+            if (childAdapterPosition == 4) {
+                outRect.top = 25
+            }
+//            outRect.top = 13
+//            outRect.bottom = 25
 //            outRect.left = 130
 //            outRect.right = 30
         }
@@ -57,26 +58,46 @@ class ScratchActivity : AppCompatActivity() {
             val childCount = parent.childCount
             Log.i("AAA", "child count: $childCount")
 
-            for (i in 0..childCount-1) {
-                Log.d("AAA", "child $i")
-                val child = parent.getChildAt(i)
-                val prioTxt = (parent.findContainingViewHolder(child) as ShopItemViewHolder?)
-                        ?.priorityText
-                        ?: "NO VIEW HOLDER"
-                Log.d("AAA", "priority text: $prioTxt")
-
-                if (prioTxt == "1") continue
-
-                val bounds = Rect()
-                        .apply { parent.getDecoratedBoundsWithMargins(child, this) }
-                val left = 10
-                val top = bounds.top + 1
-                val right = left + 200
-                val bottom = bounds.bottom - 1
-
-                val rect = Rect(left, top, right, bottom)
-                c.drawRect(rect, paint)
+            if (childCount >= 4) {
+                val child4 = parent.getChildAt(4)
+                val child4Bounds = Rect()
+                parent.getDecoratedBoundsWithMargins(child4, child4Bounds)
+                val paint = Paint().apply {
+                    style = Paint.Style.STROKE
+                    strokeWidth = 10f
+                    color = Color.GRAY
+                    strokeCap = Paint.Cap.ROUND
+                    pathEffect = DashPathEffect(floatArrayOf(30f, 50f), 0f)
+                }
+                val verticalPosition = child4Bounds.top + 13.5f
+                val path = Path().apply {
+                    moveTo(10f, verticalPosition)
+                    lineTo(child4.width - 20f, verticalPosition)
+                }
+                c.drawPath(path, paint)
             }
+
+            // the following is an example of how to draw behind a recycleview item
+//            for (i in 0..childCount-1) {
+//                Log.d("AAA", "child $i")
+//                val child = parent.getChildAt(i)
+//                val prioTxt = (parent.findContainingViewHolder(child) as ShopItemViewHolder?)
+//                        ?.priorityText
+//                        ?: "NO VIEW HOLDER"
+//                Log.d("AAA", "priority text: $prioTxt")
+//
+//                if (prioTxt == "1") continue
+//
+//                val bounds = Rect()
+//                        .apply { parent.getDecoratedBoundsWithMargins(child, this) }
+//                val left = 10
+//                val top = bounds.top + 1
+//                val right = left + 200
+//                val bottom = bounds.bottom - 1
+//
+//                val rect = Rect(left, top, right, bottom)
+//                c.drawRect(rect, paint)
+//            }
 
             c.restore()
         }
