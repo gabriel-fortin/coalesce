83f7a063a528e4adb6809b9b74e21b3d4227a149
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2018-03-30 19:18:23 +0100

    improve ShoppingsViewHolder

--- app/src/main/java/com/example/g14/coalesce/app/shoppinglist/internal/ShoppingsViewHolder.kt
+++ app/src/main/java/com/example/g14/coalesce/app/shoppinglist/internal/ShoppingsViewHolder.kt
@@ -5,11 +5,20 @@ import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import com.example.g14.coalesce.app.R
 
-class ShoppingsViewHolder(view: View) : RecyclerView.ViewHolder(view), SwipeItemForOptions.ItemHelper {
+/**
+ * For a view in a recycler's item view this class exposes:
+ *  - the view itself, or
+ *  - fields of interest from that view.
+ *
+ * This class does not (should not) set any listeners or other logic; just expose its content.
+ */
+class ShoppingsViewHolder(view: View)
+    : RecyclerView.ViewHolder(view), SwipeItemForOptions.ItemHelper {
     companion object {
         val tag: String = ShoppingsViewHolder::class.java.simpleName
     }
@@ -17,17 +26,21 @@ class ShoppingsViewHolder(view: View) : RecyclerView.ViewHolder(view), SwipeItem
     private val prioTextTV = view.findViewById(R.id.priorityText) as TextView
     private val titleTextTV = view.findViewById(R.id.titleText) as TextView
     private val checkBoxCB = view.findViewById(R.id.buyingStateBox) as CheckBox
-    private val buttonsEnding = view.findViewById(R.id.buttonsEnding) as View
+    private val underbuttonsEnding = view.findViewById(R.id.buttonsEnding) as View
+
     val itemData = view.findViewById(R.id.itemData) as ViewGroup
 
+    val reorderUnderbutton = view.findViewById(R.id.reorderButton) as ImageButton
+    val deleteUnderbutton = view.findViewById(R.id.removeButton) as ImageButton
 
     init {
-        view.findViewById<View>(R.id.reorderButton).setOnClickListener {
+        reorderUnderbutton.setOnClickListener {
             val msg = "sort CLICK"
             Log.d(tag, msg)
             Toast.makeText(view.context, msg, Toast.LENGTH_SHORT).show()
         }
-        view.findViewById<View>(R.id.removeButton).setOnClickListener {
+        // TODO: delete toast when real implementation is in place
+        deleteUnderbutton.setOnClickListener {
             val msg = "remove CLICK"
             Log.d(tag, msg)
             Toast.makeText(view.context, msg, Toast.LENGTH_SHORT).show()
@@ -44,13 +57,11 @@ class ShoppingsViewHolder(view: View) : RecyclerView.ViewHolder(view), SwipeItem
         get() = titleTextTV.text.toString()
 
     var checkBox: Boolean = false
-//    var checkBox: Boolean
         set(value) {
             checkBoxCB.isChecked = value
         }
-//        get() = checkBoxCB.isChecked
 
     override fun getViewToSwipe(): ViewGroup = itemData
 
-    override fun getMaxSwipeDistance(): Float = buttonsEnding.left.toFloat()
+    override fun getMaxSwipeDistance(): Float = underbuttonsEnding.left.toFloat()
 }
